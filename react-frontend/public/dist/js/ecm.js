5b9f11d0d6106cff024f56217e119d7628e2fdda
Curtis Mumby <curtis.mumby@gmail.com>
2018-08-26 23:03:18 -0400

    More Case Menu Toggle functioinality

--- react-frontend/public/dist/js/ecm.js
+++ react-frontend/public/dist/js/ecm.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
     $('.cip-toggle').click(function(){
-       $('form[name=reqForm] > .box-body').each(function(){
+        
+        $('.case-menu li').removeClass('active');
+        $(this).parent().addClass('active');
+        $('form[name=reqForm] > .box-body').each(function(){
             if(!$(this).hasClass('cip')){
                 $(this).css("display", "none");
+            } else{
+                $(this).css("display", "block"); 
             }
-       });
+        });
     });
+
+    $('.proxyrr-toggle').click(function(){
+        $('.case-menu li').removeClass('active');
+        $(this).parent().addClass('active');
+        $('form[name=reqForm] > .box-body').each(function(){
+             if(!$(this).hasClass('proxyrr')){
+                 $(this).css("display", "none");
+             } else{
+                $(this).css("display", "block"); 
+            }
+        });
+     });
 });
--- react-frontend/src/components/CaseMenu.js
+++ react-frontend/src/components/CaseMenu.js
@@ -106,10 +106,10 @@ export default class CaseMenu extends Component {
                     </div>
                 </div>
                 <div className="box-body no-padding">
-                    <ul className="nav nav-pills nav-stacked">
-                    <li className="active"><a href="#"><i className="fa fa-exclamation-triangle"></i> Proxy RR Requirements
+                    <ul className="case-menu nav nav-pills nav-stacked">
+                    <li className="active"><a href="#proxyrr" className="proxyrr-toggle"><i className="fa fa-exclamation-triangle"></i> Proxy RR Requirements
                         <span className="label label-primary pull-right">12</span></a></li>
-                    <li><a href="#" className="cip-toggle" ><i className="fa fa-user-circle"></i> CIP Requirements</a></li>
+                    <li><a href="#cip" className="cip-toggle" ><i className="fa fa-user-circle"></i> CIP Requirements</a></li>
                     <li><a href="#"><i className="fa fa-wrench"></i> Remediation Requirements</a></li>
                     <li><a href="#"><i className="fa fa-users"></i> Related Parties</a></li>
                     <li><a href="#"><i className="fa fa-search"></i> Screening <span className="label label-warning pull-right">65</span></a>
