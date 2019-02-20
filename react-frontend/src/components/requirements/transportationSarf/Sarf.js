430a7a521e4b11cac5cad9a8479b0c68b36bfd24
Curtis Mumby <curtis.mumby@gmail.com>
2019-02-20 13:17:22 -0500

    Case Menu Highlights now update on the fly for  Transportation SARF

--- react-frontend/src/components/requirements/transportationSarf/Sarf.js
+++ react-frontend/src/components/requirements/transportationSarf/Sarf.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { getSectionStatuses } from '../../../util/getSectionStatuses';
 import CaseService from '../../CaseService';
 import CaseStructure from '../../structures/CaseStructure';
 import Location from '../../../util/Location';
+import sectionCompleteStatus from '../../../util/sectionCompleteStatus';
 
 
-export default class Sarf extends Component {
+class Sarf extends Component {
     
     constructor(props) {
         super(props);
@@ -25,15 +28,6 @@ export default class Sarf extends Component {
 
     componentWillMount() {
         this.fillData();
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot){ 
-        let updatedCase = prevState.case;
-        this.updateData(this.props.case);
-       if (updatedCase.requirement.hasOwnProperty('cip')){
-       } else {
-         return false;
-       }
     }
 
     //Routes the changed information to the right poperty
@@ -63,12 +57,22 @@ export default class Sarf extends Component {
     }
 
     updateForm = (event, name) => {
+        const {ecmId, requirement} = this.props.case; 
         this.handleFormDataRouting(event, name);
         if(name === "srf-correction-required" || "srf-complete"){
             this.setState({[name]: event.target.checked});
         }  else{
             this.setState({[name]: event.target.value});
         }
+
+        if(name === "srf-complete"){
+            const isComplete = sectionCompleteStatus(ecmId, requirement.transportationSarf);
+            let newStatus = getSectionStatuses(requirement);
+            newStatus.transportationSarf = isComplete;
+            this.props.onSectionStatusFill(newStatus);
+        }
+
+        this.updateData(this.props.case);
     }
 
     render() { 
@@ -135,3 +139,20 @@ export default class Sarf extends Component {
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+      statuses: state.sectionStatuses,
+    };
+};
+
+const mapDispatchToProps = dispatch => { 
+    return {
+        onSectionStatusFill: (statuses) => dispatch({type:"STATUS_UPDATE", value: statuses})
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Sarf);
